f2e58f6c5155b0da869a55c46c21ab86cfee1d72
shaftmind <adaias.almeida@bandtec.com.br>
2022-01-11 18:11:15 -0300

    add: incio projeto

---
+++ HyperCar-Project/src/main/java/br/com/hyper/HyperCar/HyperCarApplication.java
@@ -0,0 +1,13 @@
+package br.com.hyper.HyperCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyperCarApplication.class, args);
+	}
+
+}
---
+++ HyperCar-Project/src/test/java/br/com/hyper/HyperCar/HyperCarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hyper.HyperCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyperCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
