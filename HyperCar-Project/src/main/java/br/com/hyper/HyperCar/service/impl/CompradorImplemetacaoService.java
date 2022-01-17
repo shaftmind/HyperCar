efc8e1812df3a1e168508b542d8198c180c85449
samuel-prazeres-junior <samuel.prazeresjr@bandtec.com.br>
2022-01-17 11:05:40 -0300

    Adicionando Exception

--- HyperCar-Project/src/main/java/br/com/hyper/HyperCar/service/impl/CompradorImplemetacaoService.java
+++ HyperCar-Project/src/main/java/br/com/hyper/HyperCar/service/impl/CompradorImplemetacaoService.java
@@ -1,20 +1,18 @@
 package br.com.hyper.HyperCar.service.impl;
 
-import br.com.hyper.HyperCar.Entity.Carro;
 import br.com.hyper.HyperCar.Entity.Comprador;
-import br.com.hyper.HyperCar.Entity.Endereco;
-import br.com.hyper.HyperCar.Repository.CarroRepository;
 import br.com.hyper.HyperCar.Repository.CompradorRepository;
+import br.com.hyper.HyperCar.exception.ResourceNotFoundException;
 import br.com.hyper.HyperCar.service.ICompradorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CompradorImplemetacaoService implements ICompradorService {
 
+    @Autowired
     private CompradorRepository compradorRepository;
 
     @Override
@@ -24,11 +22,8 @@ public class CompradorImplemetacaoService implements ICompradorService {
 
     @Override
     public Comprador buscarComprador(Integer id) {
-        Optional<Comprador> endereco = compradorRepository.findById(id);
-        if (endereco.isPresent()){
-            return endereco.get();
-        }
-        return null;
+        return compradorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Nenhum registro encontrado para este ID"));
     }
 
     @Override
@@ -38,9 +33,9 @@ public class CompradorImplemetacaoService implements ICompradorService {
 
     @Override
     public void editarComprador(Comprador car, Integer id) {
-        Optional<Comprador> compradorAtualizar = compradorRepository.findById(id);
-        if(compradorAtualizar.isPresent()){
-            Comprador compradorAtualizado = compradorAtualizar.get();
+        Comprador compradorAtualizado = compradorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Nenhum registro encontrado para este ID"));
+
             compradorAtualizado.setAtivo(true);
             compradorAtualizado.setCpf(car.getCpf());
             compradorAtualizado.setDataNascimento(car.getDataNascimento());
@@ -48,16 +43,16 @@ public class CompradorImplemetacaoService implements ICompradorService {
             compradorAtualizado.setNome(car.getNome());
             compradorAtualizado.setSobrenome(car.getSobrenome());
             compradorRepository.save(compradorAtualizado);
-        }
+
     }
 
     @Override
     public void deletarComprador(Integer id) {
-        Optional<Comprador> compradorASerDeletado = compradorRepository.findById(id);
-        if (compradorASerDeletado.isPresent()){
-            Comprador compradorDeletado = compradorASerDeletado.get();
+        Comprador compradorDeletado = compradorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Nenhum registro encontrado para este ID"));
+
             compradorDeletado.setAtivo(false);
             compradorRepository.save(compradorDeletado);
-        }
+
     }
 }
